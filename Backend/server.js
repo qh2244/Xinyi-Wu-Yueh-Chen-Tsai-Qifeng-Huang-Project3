bbffb49cfc28a4623b30089b994798c4bc94ae9d
Krystal Wu <kk@Krystals-MacBook-Air.local>
2023-12-13 15:36:23 -0800

    Finished Backend APIs

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const userRouter = require("./router/user")
+const PostRouter = require("./Modals/Post")
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB).then(()=>
@@ -16,6 +17,7 @@ console.log("DB connection successfull")).catch(()=>{
 
 app.use(express.json())
 app.use("/api/user", userRouter);
+app.use("/api/post", PostRouter);
 
 app.listen(8000, ()=>{
     console.log("Server is running")
